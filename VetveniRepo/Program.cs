9505fd78e1910494dbf20eea8e25e87e8da5ed49
michaela.loffelmann <michaela.loffelmann@VT1-11>
2023-03-20 10:08:08 +0100

    Přidejte soubory projektu.

---
+++ VetveniRepo/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.ForegroundColor= ConsoleColor.Cyan;
+
+string[] predmetyPrvak = {
+    "ALP," + "AJ, " + "ČJ, " + "D," + "HW," + "ZE," + "OPŘ," + "ZEL," + "CH," + "M," + "IKT," + "F" + "OS" + "TV" };
+
+Console.WriteLine("vyber ročník 1 - 4");
+string vyberUzivatele = Console.ReadLine();
+
+switch(vyberUzivatele)
+{
+    case "1":
+        foreach (var item in predmetyPrvak) 
+        { 
+            Console.WriteLine(item); 
+        }
+        break;
+    default: 
+        Console.WriteLine("Coming soon :)");
+        break;
+}
